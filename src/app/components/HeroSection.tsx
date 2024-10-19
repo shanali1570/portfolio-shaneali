65f97a6e9324226df3d1902416f994202ed60da0
shanali1570 <mrshanali00786@gmail.com>
2024-10-19 08:48:22 +0500

    updated

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -15,8 +15,8 @@ const HeroSection: React.FC = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
-            <span className=" bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+          <h1 className="text-white mb-6 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className=" bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600 mb-2">
               Hello, I&apos;m{" "}
             </span>
             <br />
