76f908b7e4b5ed181f3ada8542dc91c3efc2b240
shanali1570 <mrshanali00786@gmail.com>
2024-10-20 22:22:41 +0500

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,15 +17,15 @@ const navLinks: NavLink[] = [
   },
   {
     title: "About",
-    path: "/about",
+    path: "#about",
   },
   {
     title: "Projects",
-    path: "/projects",
+    path: "#projects",
   },
   {
     title: "Contact",
-    path: "/contact",
+    path: "#contact",
   },
 ];
 
